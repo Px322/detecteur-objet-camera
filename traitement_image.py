8857ad5b63e937526c684611cfa3bdb39d1c702f
Paul Rousseau <prousseauu@gmail.com>
2026-02-23 23:52:10 +0100

    Deuxieme commit - recup flux video

--- traitement_image.py
+++ traitement_image.py
@@ -7,5 +7,15 @@ capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 if not capture.isOpened():
     print("Impossible d'ouvrir la caméra")
     exit()
+while True:
+    ret, frame = capture.read()
 
+    if not ret:
+        print("Impossible de recevoir les frames de la vidéo")
+        break
+    cv2.imshow("Caméra",frame)
 
+    if cv2.waitKey(1) == ord('q'):
+        break
+capture.release()
+cv2.destroyAllWindows()
