039989eb787e44f5c23799489d6dbb9db5ef15dd
Px322 <prousseauu@gmail.com>
2026-02-24 22:39:10 +0100

    Troisieme commit - modif frame

--- traitement_image.py
+++ traitement_image.py
@@ -9,6 +9,11 @@ if not capture.isOpened():
     exit()
 while True:
     ret, frame = capture.read()
+    frame = cv2.resize(frame,(224,224))
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Inversement caneaux couleurs BGR (base de opencv) à RGB
+    frame_array = img_array = np.expand_dims(frame, axis=0)
+    img_preprocessed = tf.preprocess_input(frame_array)
+    predictions = model.predict(img_preprocessed)
 
     if not ret:
         print("Impossible de recevoir les frames de la vidéo")
